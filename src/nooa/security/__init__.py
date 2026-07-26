32c5a5ce155993a8f78b3e04680d3f25fd8a7fda
nrosen <nrosen@nvidia.com>
2026-07-26 14:37:24 -0700

    Add security receipt transport schema

--- src/nooa/security/__init__.py
+++ src/nooa/security/__init__.py
@@ -4,10 +4,12 @@
 
 from nooa.security.effects import EffectRecord
 from nooa.security.install import EffectObservation, EffectObserver, install_effect_recorder
+from nooa.security.receipts import SecurityReceipt
 
 __all__ = [
     "EffectObservation",
     "EffectObserver",
     "EffectRecord",
+    "SecurityReceipt",
     "install_effect_recorder",
 ]
